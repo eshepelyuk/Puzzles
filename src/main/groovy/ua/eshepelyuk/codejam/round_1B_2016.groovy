b35a8555b73546261f63b9d2e0d6b21f75d58233
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2016-06-03 17:03:56 +0300

    skeleton

---
+++ src/main/groovy/ua/eshepelyuk/codejam/round_1B_2016.groovy
@@ -0,0 +1,10 @@
+package ua.eshepelyuk.codejam
+
+/**
+ * https://code.google.com/codejam/contest/11254486/dashboard#s=p0
+ */
+class round_1B_2016 {
+    static def gettingTheDigits() {
+        null
+    }
+}
---
+++ src/test/groovy/ua/eshepelyuk/codejam/round_1B_2016Test.groovy
@@ -0,0 +1,11 @@
+package ua.eshepelyuk.codejam
+
+import spock.lang.Specification
+
+class round_1B_2016Test extends Specification {
+    def "test gettingTheDigits"() {
+        given: ''
+        when: ''
+        then: ''
+    }
+}
